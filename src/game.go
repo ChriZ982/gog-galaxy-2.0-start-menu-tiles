34179b1015df8dcfa3f9d63fedfc823e8e31b8f1
ChriZ982 <christopher.zinda2@googlemail.com>
2020-04-27 19:28:11 +0200

    Improved Game code

---
+++ src/game.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// Game contains important data of one game that is needed for tiles
+type Game struct {
+	FileName     string
+	Title        string
+	ReleaseKey   string
+	IconFileName string
+}
+
+// Sanitize cleans disallowed characters from all strings
+func (g *Game) Sanitize(disallowedChars *regexp.Regexp) {
+	g.FileName = disallowedChars.ReplaceAllString(g.ReleaseKey, "")
+	g.Title = disallowedChars.ReplaceAllString(strings.Split(g.Title, ":")[1], "")
+	g.IconFileName = strings.ReplaceAll(g.IconFileName, ".webp", ".png")
+}
+
+// ExistsIn searches for current game in a list
+func (g *Game) ExistsIn(games []Game) bool {
+	for _, game := range games {
+		if g.Title == game.Title {
+			log.Debugf("'%s' already exists with ReleaseKey '%s'", game.Title, game.ReleaseKey)
+			return true
+		}
+	}
+	return false
+}
